1dc0e2e876b7ba8766d30a97f77801fe605f50cd
Okey ifeanyi <101342170+okeyifea@users.noreply.github.com>
2022-05-19 11:44:25 -0700

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _write_char - writes the character c to stdout
@@ -7,7 +7,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _write_char(char c)
+int _putchar(char *c)
 {
 	return (write(1, &c, 1));
 }
